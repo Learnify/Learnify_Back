88e42d982a30b91b891958d159485e73c961d10c
Julian Moreno <bjmorenon@unal.eu.co>
2018-10-09 14:39:01 -0500

    Arreglo de error de Notificaciones
    Existía un error al intentar crear usuarios, faltaba un flash que permitía notificar la creación de usuarios o errores que se presentan.

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,11 @@ module LearnifyBackEnd
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
+    config.api_mode = false
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000', 'https://desolate-waters-78828.herokuapp.com/' #replace this url with that of your own heroku client app
